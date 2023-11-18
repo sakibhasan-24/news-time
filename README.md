d76be23a2c0187ec2528a4050162b34453c9da9c
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-11-18 20:28:48 +0600

    install and page setup

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-# React + Vite
+# React + Router + Tailwind.css + firebase + daisi ui
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+###### install all package
 
-Currently, two official plugins are available:
+# page structure
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+- routes files
+- firebase files
+- pages files
---
+++ src/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTvGxDHG4Hy6kNwJqMvTg5IlZNJp_OIAc",
+  authDomain: "news-time-30c13.firebaseapp.com",
+  projectId: "news-time-30c13",
+  storageBucket: "news-time-30c13.appspot.com",
+  messagingSenderId: "383428256269",
+  appId: "1:383428256269:web:10c90eb7e6cf143f9bab44",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
